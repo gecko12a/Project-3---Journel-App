310bca14de1b3cf4cf5564bc36bd3d82977c007d
gecko12a <gecko12a@yahoo.com>
2020-04-03 16:16:52 -0400

    arrary split error fixed

--- server/app.js
+++ server/app.js
@@ -17,33 +17,33 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/DomoMaker';
 
 const mongooseOptions = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
 
-mongoose.connect(dbURL, mongooseOptions,(err) => {
-    if(err){
-        console.log('Could not connect to database');
-        throw err;
-    }
+mongoose.connect(dbURL, mongooseOptions, (err) => {
+  if (err) {
+    console.log('Could not connect to database');
+    throw err;
+  }
 });
 
 let redisURL = {
-    hostname: 'redis-14670.c114.us-east-1-4.ec2.cloud.redislabs.com',
-    port: 14670,
+  hostname: 'redis-14670.c114.us-east-1-4.ec2.cloud.redislabs.com',
+  port: 14670,
 };
 
 let redisPASS = 'biPmoBwM0FXRJX9cbQRcar9Qynpt58dg';
 
 if (process.env.REDISCLOUD_URL) {
-    redisURL = url.parse(process.env.REDISCLOUD_URL);
-    redisPASS = redisURL.auth.split(':')[1];
+  redisURL = url.parse(process.env.REDISCLOUD_URL);
+  [, redisPASS] = redisURL.auth.split(':');
 }
 
-let redisClient = redis.createClient({
-    host: redisURL.hostname,
-    port: redisURL.port,
-    password: redisPASS,
+const redisClient = redis.createClient({
+  host: redisURL.hostname,
+  port: redisURL.port,
+  password: redisPASS,
 });
 
 const router = require('./router.js');
@@ -54,37 +54,37 @@ app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
 app.disable('x-powered-by');
 app.use(compression());
 app.use(bodyParser.urlencoded({
-    extended: true,
+  extended: true,
 }));
 app.use(session({
-    key: 'sessionid',
-    store: new RedisStore({
-        client: redisClient,
-    }),
-    secret: 'Domo Arigato',
-    resave: true,
-    saveUninitialized: true,
-    cookie: {
-        httpOnly: true,
-    },
+  key: 'sessionid',
+  store: new RedisStore({
+    client: redisClient,
+  }),
+  secret: 'Domo Arigato',
+  resave: true,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+  },
 }));
-app.engine('handlebars',expressHandlebars({ defaultLayout: 'main' }));
-app.set('view engine','handlebars');
+app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/../views`);
 app.use(cookieParser());
 app.use(csrf());
 app.use((err, req, res, next) => {
-    if (err.code !== 'EBADCSRFTOKEN') return next(err);
+  if (err.code !== 'EBADCSRFTOKEN') return next(err);
 
-    console.log('Missing CSRF token');
-    return false;
+  console.log('Missing CSRF token');
+  return false;
 });
 
 router(app);
 
 app.listen(port, (err) => {
-if(err){
+  if (err) {
     throw err;
-}
-console.log(`listening on port ${port}`);
+  }
+  console.log(`listening on port ${port}`);
 });
--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -1,45 +1,45 @@
 const models = require('../models');
 
-const Domo = models.Domo;
+const { Domo } = models;
 
 const makeDomo = (req, res) => {
-    if (!req.body.name || !req.body.age) {
-        return res.status(400).json({error: 'RAWR! Both name and age are required'});
+  if (!req.body.name || !req.body.age) {
+    return res.status(400).json({ error: 'RAWR! Both name and age are required' });
+  }
+
+  const domoData = {
+    name: req.body.name,
+    age: req.body.age,
+    owner: req.session.account._id,
+  };
+
+  const newDomo = new Domo.DomoModel(domoData);
+
+  const domoPromise = newDomo.save();
+
+  domoPromise.then(() => res.json({ redirect: '/maker' }));
+
+  domoPromise.catch((err) => {
+    console.log(err);
+    if (err.code === 11000) {
+      return res.status(400).json({ error: 'Domo already exists' });
     }
 
-    const domoData = {
-        name: req.body.name,
-        age: req.body.age,
-        owner: req.session.account._id,
-    };
+    return res.status(400).json({ error: 'An error occured' });
+  });
 
-    const newDomo = new Domo.DomoModel(domoData);
-
-    const domoPromise = newDomo.save();
-
-    domoPromise.then(() => res.json({redirect: '/maker' }));
-
-    domoPromise.catch((err) => {
-        console.log(err);
-        if (err.code === 11000){
-            return res.status(400).json({error: 'Domo already exists'});
-        }
-
-        return res.status(400).json({error: 'An error occured'});
-    });
-
-    return domoPromise;
-}
+  return domoPromise;
+};
 
 const makerPage = (req, res) => {
-    Domo.DomoModel.findByOwner(req.session.account._id, (err, docs) =>{
-        if (err){
-            console.log(err);
-            return res.status(400).json({error: 'An error occured'});
-        }
+  Domo.DomoModel.findByOwner(req.session.account._id, (err, docs) => {
+    if (err) {
+      console.log(err);
+      return res.status(400).json({ error: 'An error occured' });
+    }
 
-        return res.render('app', {csrfToken: req.csrfToken(), domos: docs});
-    });
+    return res.render('app', { csrfToken: req.csrfToken(), domos: docs });
+  });
 };
 
 module.exports.makerPage = makerPage;
